dd92dfb11bf7e798546024becbe5f527303455fd
DESKTOP-A0OQPON\William <orivealonso@gmail.com>
2024-04-07 12:24:18 +0200

    ADD Comments to the code

--- Assets/rotation.cs
+++ Assets/rotation.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class rotation : MonoBehaviour
 {
-    private Camera mainCamera;
     private Vector3 mousePos;
     // Start is called before the first frame update
     void Start()
     {
-        // mainCamera = GameObject.FindGameObjectWithTag("Main Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
         Vector3 rotation = mousePos - transform.position;
-        
+        // Transforms the radio of the position into degrees
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
+        // Will change the rotation position in the Z axis
         transform.rotation = Quaternion.Euler(0,0,rotZ);
     }
 }
