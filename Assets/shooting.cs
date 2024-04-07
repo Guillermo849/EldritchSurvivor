be9c91106208e208a53489606c05133f4515225c
DESKTOP-A0OQPON\William <orivealonso@gmail.com>
2024-04-07 19:09:56 +0200

    MOD rotation script name to shooting

--- Assets/shooting.cs
+++ Assets/shooting.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotation : MonoBehaviour
+public class shooting : MonoBehaviour
 {
     private Vector3 mousePos;
+    // public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +24,18 @@ public class rotation : MonoBehaviour
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         // Will change the rotation position in the Z axis
         transform.rotation = Quaternion.Euler(0,0,rotZ);
+
+        if (!canFire) {
+            timer += Time.deltaTime;
+            if (timer > timeBetweenFiring) {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && canFire) {
+            canFire = false;
+            // Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
     }
 }
